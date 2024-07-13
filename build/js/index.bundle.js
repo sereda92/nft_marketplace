79f0379b1c1f859fc558446ea4609bf8092b5bbe
sereda_pavel <assakay92@gmail.com>
2024-07-13 11:04:01 +0300

    burger main

--- build/js/index.bundle.js
+++ build/js/index.bundle.js
@@ -1,1 +1,1 @@
-!function(){"use strict";const e=document.querySelector(".burger__btn"),t=document.querySelector(".main");e.addEventListener("click",(()=>{t.classList.toggle("main_active")}))}();
+!function(){"use strict";const e=document.querySelector(".burger__btn"),t=document.querySelector(".main"),c=document.querySelector("body"),r=document.querySelector(".burger");e.addEventListener("click",(()=>{t.classList.toggle("main_active"),c.classList.toggle("body_active"),r.classList.toggle("burger__active"),e.classList.toggle("burger__btn_active")}))}();
--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,12 @@ import burger from './burger.js'
 
 const burgerBtn = document.querySelector('.burger__btn')
 const main = document.querySelector('.main')
+const body = document.querySelector('body')
+const burgerBody = document.querySelector('.burger')
 
 burgerBtn.addEventListener('click', ()=> {
     main.classList.toggle('main_active')
+    body.classList.toggle('body_active')
+    burgerBody.classList.toggle('burger__active')
+    burgerBtn.classList.toggle('burger__btn_active')
 })
